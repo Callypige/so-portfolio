8805160f50a48ba43ac13460daa24065db4ddeae
Callypige <sophie.nourry58@outlook.fr>
2026-03-19 00:11:24 +0100

    Add sitemap function to define site URLs and last modified dates

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sonourrypayn.dev",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sonourrypayn.dev/cv",
+      lastModified: new Date(),
+    },
+  ];
+}
